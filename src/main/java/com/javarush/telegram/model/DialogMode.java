493a09df0583a5b16ad9b2b9bc060af7d757ba7a
bogomolova <rospisdyaika@gmail.com>
2024-11-17 22:53:50 +0200

    initial commit

---
+++ src/main/java/com/javarush/telegram/model/DialogMode.java
@@ -0,0 +1,10 @@
+package com.javarush.telegram.model;
+
+public enum DialogMode {
+    MAIN,
+    PROFILE,
+    OPENER,
+    MESSAGE,
+    DATE,
+    GPT,
+}
---
+++ src/main/java/com/javarush/telegram/model/TinderBoltApp.java
@@ -0,0 +1,27 @@
+package com.javarush.telegram.model;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.objects.*;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class TinderBoltApp extends MultiSessionTelegramBot {
+    public static final String TELEGRAM_BOT_NAME = "bot-name"; //TODO: добавь имя бота в кавычках
+    public static final String TELEGRAM_BOT_TOKEN = "bot-token"; //TODO: добавь токен бота в кавычках
+    public static final String OPEN_AI_TOKEN = "chat-gpt-token"; //TODO: добавь токен ChatGPT в кавычках
+
+    public TinderBoltApp() {
+        super(TELEGRAM_BOT_NAME, TELEGRAM_BOT_TOKEN);
+    }
+
+    @Override
+    public void onUpdateEventReceived(Update update) {
+        //TODO: основной функционал бота будем писать здесь
+
+    }
+
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(new TinderBoltApp());
+    }
+}
