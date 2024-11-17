617cff99bb7aa6eb188443841af48101e5c76911
bogomolova <rospisdyaika@gmail.com>
2024-11-17 23:45:48 +0200

    Welcome to the chat

--- src/main/java/com/javarush/telegram/model/TinderBoltApp.java
+++ src/main/java/com/javarush/telegram/model/TinderBoltApp.java
@@ -6,8 +6,8 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class TinderBoltApp extends MultiSessionTelegramBot {
-    public static final String TELEGRAM_BOT_NAME = "bot-name"; //TODO: добавь имя бота в кавычках
-    public static final String TELEGRAM_BOT_TOKEN = "bot-token"; //TODO: добавь токен бота в кавычках
+    public static final String TELEGRAM_BOT_NAME = "chat_gpt_and_connection_aibot";
+    public static final String TELEGRAM_BOT_TOKEN = "7698332175:AAE5aOoCuAoDYjKDOBTGalOorQ2VZRKfdVM";
     public static final String OPEN_AI_TOKEN = "chat-gpt-token"; //TODO: добавь токен ChatGPT в кавычках
 
     public TinderBoltApp() {
@@ -16,8 +16,17 @@ public class TinderBoltApp extends MultiSessionTelegramBot {
 
     @Override
     public void onUpdateEventReceived(Update update) {
-        //TODO: основной функционал бота будем писать здесь
+        String message = getMessageText();
+        if (message.equals("/start")) {
+            sendPhotoMessage("main");
+            String messageText = loadMessage("main");
+            sendTextMessage(messageText);
+            return;
+        }
 
+        sendTextMessage("Здравствуйте, я *ChatBot | AI | GPT*");
+        sendTextButtonsMessage("\n_Рассказатть вам что я умею?_",
+                "Да", "Yes", "Нет", "No");
     }
 
     public static void main(String[] args) throws TelegramApiException {
